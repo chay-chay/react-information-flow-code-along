e81ec4f3f360aa7b978c9208af95e99048a3c802
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-02-25 20:20:07 -0500

    commit tree 76239a3cf4a3f474720ddea02e6192b71bec7a36

--- src/randomColorGenerator.js
+++ src/randomColorGenerator.js
@@ -1,3 +1,4 @@
+
 // Nothing in this file needs to be altered (but it is your solution so feel free to!)
 export function getRandomColor() {
   // this function generates a random hex color from the letters below
@@ -8,3 +9,26 @@ export function getRandomColor() {
   }
   return color
 }
+// class Parent extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       color: getRandomColor()
+//     }
+//   }
+
+//   changeColor = () => {
+//     this.setState({
+//       color: getRandomColor()
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="parent" style={{backgroundColor: this.state.color}}>
+//         <Child handleColorChange={this.changeColor}/>
+//         <Child handleColorChange={this.changeColor}/>
+//       </div>
+//     )
+//   }
+// } 
